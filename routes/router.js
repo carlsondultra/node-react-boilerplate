ca66b37b1b62f01bca146ace40f5e83d76d5a990
carlsondultra <carlsondultra@users.noreply.github.com>
2023-04-04 20:41:16 -0400

    adding psets route as GET endpoint

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const test = require('./api/test');
 const celllines = require('./api/celllines')
+const psets = require('./api/psets')
 
 router.get('/test', test.test);
 router.get('/celllines', celllines.get);
+router.get('/psets', psets.get);
 
 
 module.exports = router;
