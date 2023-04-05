0e284e63d7b05aa3a5f7251bc073a04c4684e210
carlsondultra <carlsondultra@users.noreply.github.com>
2023-04-04 21:22:37 -0400

    added front-end routing for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import CellLines from './Components/CellLines';
+import PSets from './Components/PSets';
+import PSetsTable from './Components/PSetsTable';
+import React from 'react';
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <Router>
+    <React.Fragment>
+      <Router>
+      <Navbar/>
       <Routes>
         <Route path='/' exact element={<Home />} />
         <Route path='/celllines' exact element={<CellLines />} />
+        <Route path='/psets' exact element={<PSets />} />
+        <Route path='/psetstable' exact element={<PSetsTable />} />
       </Routes>
     </Router>
+		</React.Fragment>
   );
 }
 
